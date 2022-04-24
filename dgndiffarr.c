8fd1c5666b7be7759d7be6f69a0b38ab32cf93c6
HariniRajmohan <101508935+HariniRajmohan@users.noreply.github.com>
2022-04-24 14:40:12 +0000

    Difference of diagonals in a matrix

---
+++ dgndiffarr.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+  int i,j,r,c,sum=0,sum1=0,a[10][10],abD=0;
+  printf("Enter the number of rows and columns:\n");
+  scanf("%d %d",&i,&j);
+  printf("Enter the elements in matrix:\n");
+  for(r=0;r<i;r++)
+    {
+      for(c=0;c<j;c++)
+        {
+          scanf("%d",&a[r][c]);
+        }
+    }
+  for(r=0;r<i;r++)
+    {
+      for(c=0;c<j;c++)
+        {
+          if(r==c)
+          {
+            sum=sum+a[r][c];
+          }
+          if(r+c==i-1)
+          {
+            sum1=sum1+a[r][c];
+          }
+        }
+    }
+  printf("The sum of diagonals is %d %d\n",sum,sum1);
+  abD = abs(sum-sum1);
+  printf("Difference is %d",abD);
+  return 0;
+}
