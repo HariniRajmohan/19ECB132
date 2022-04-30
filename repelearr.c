5fb6f0873582b64df0fdfb8f2413035ca5c467c9
HariniRajmohan <101508935+HariniRajmohan@users.noreply.github.com>
2022-04-30 01:50:06 +0000

    dividing k by pairs in array.

--- repelearr.c
+++ repelearr.c
@@ -1,42 +1,30 @@
 #include<stdio.h>
-//Program to count most occuring element
-int getMaxRepeatingElement(int array[], int n) 
+void repeating_element(int arr[], int n)
 {
-    int i, j, maxElement, count;
-     int maxCount = 0;
-    /* Frequency of each element is counted and checked.If it's greater than the utmost count element we found till now, then it is updated accordingly  */  
-    for(i = 0; i< n; i++)   //For loop to hold each element
-    {
-        count = 1;
-        for(j = i+1; j < n; j++)  //For loop to check for duplicate elements
-        {
-            if(array[j] == array[i])
-            {
-                count++;     //Increment  count
-                /* If count of current element is more than 
-                maxCount, then update maxElement */
-                if(count > maxCount)
-                {
-                    maxElement = array[j];
-                }
-            }
-        }
-    }
-    return maxElement;
+int i, j;
+int count = 0;
+printf("Repeating elements are: ");
+for(i = 0;i < n;i++)
+{
+for(j = i+1;j < n;j++)
+{
+if(arr[i] == arr[j])
+{
+printf("%d ", arr[j]);
 }
-//Driver Program
+}
+}
+}
 int main()
 {
-    int n;              //Array Size Declaration
-    printf("Enter the number of elements ");
-    scanf("%d",&n);    
-    int array[n];      //Array Declaration
-    printf("Enter the array elements");
-    for(int i=0;i<n;i++)   //Initializing Array Elements
-    {
-        scanf("%d",&array[i]);
-    } 
-    int maxElement = getMaxRepeatingElement(array, n);    //Function call
-    printf("\n Maximum Repeating Element : %d",maxElement);   //Prints the most occuring element 
-    return 0;
+int n;
+scanf("%d",&n);
+int arr[n];
+int i;
+for(i = 0; i < n; i++)
+{
+scanf("%d",&arr[i]);
+}
+repeating_element(arr,n);
+return 0;
 }
